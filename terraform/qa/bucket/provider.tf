1001ae18fc4249622b92e30d77b24b0426648347
NARUTO <moises@example.com>
2025-11-07 08:36:37 +0100

    g

--- terraform/qa/bucket/provider.tf
+++ terraform/qa/bucket/provider.tf
@@ -7,7 +7,6 @@ provider "google" {
   project           = local.gcp_project_id              # GCP Project ID
   region            = local.gcp_region                  # GCP Region
   default_labels    = {
-    environment     = "qa"  
-    tags            = local.gcp_tags                         # Default label for the environment
+    environment     = "qa"                          # Default label for the environment
   }
 }
